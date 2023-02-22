c3ec7a4e6e19d84b983633003b6f50e0baf93b35
Carlos Ernesto González Flores <cgf72793@udelasalle.edu.mx>
2023-02-21 18:48:18 -0600

    Segundo Mapa

---
+++ main.js
@@ -0,0 +1,17 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [-11322460.087016312,2410083.6458656345],
+    zoom: 18
+  })
+});
